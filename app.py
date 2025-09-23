52ed95041403514a6ec688a2ad11db1866a7a779
N6SYNTAX <127181966+N6SYNTAX@users.noreply.github.com>
2025-09-23 21:25:12 +1000

    Progressive lineup builder

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import sys   
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QLineEdit,  QFormLayout, QComboBox, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QLineEdit,  QFormLayout, QComboBox, QVBoxLayout, QHBoxLayout
 from PyQt6.QtCore import Qt
 
 #----- TEST DATA
-TeamTestData = ["C Grade", "C Reserve", "D Grade", "D Reserve", "Womens"]
-PlayerTestData = ["Will David", "David Teakle", "Angus O'loughlin", "Grant Stacomb", "Sheran Medelicot"]
-Pos = ["P", "C", "1B", "2B", "3B", "SS", "LF", "CF", "RF", "DP", "DH"]
+TeamTestData = ["Select Team", "C Grade", "C Reserve", "D Grade", "D Reserve", "Womens"]
+PlayerTestData = ["Select Player","Will David", "David Teakle", "Angus O'loughlin", "Grant Stacomb", "Sheran Medelicot"]
+Pos = ["Select Position", "P", "C", "1B", "2B", "3B", "SS", "LF", "CF", "RF", "DP", "DH"]
 
 
 class Scorecard(QWidget):
@@ -13,52 +13,57 @@ class Scorecard(QWidget):
         super().__init__()
 
         self.setWindowTitle("DCBC")
-        
-
-        layout = QVBoxLayout(self)
-        #self.setLayout(layout)
-
+        self.outer = QVBoxLayout(self)
 
         # Select Team
-        layout.addWidget(QLabel("Team:"))
-        self.team = QComboBox()
-        self.team.addItems(TeamTestData)
-        self.team.setCurrentIndex(1)
-        layout.addWidget(self.team)
+        teamrow = QHBoxLayout()
+        teamrow.addWidget(QLabel("Team:"))
 
-        # Select Players
-        layout.addWidget(QLabel("1:"))
-        self.player1 = QComboBox()
-        self.player1.addItems(PlayerTestData)
-        self.player1.setCurrentIndex(1)
-        layout.addWidget(self.player1)
+        self.teambox = QComboBox()
+        self.teambox.addItems(TeamTestData)
+        teamrow.addWidget(self.teambox)
 
-        self.combo = QComboBox()
-        self.combo.addItems(Pos)
-        self.combo.setCurrentIndex(1)
-        layout.addWidget(self.combo)
-
-
-
+        self.outer.addLayout(teamrow)
         
-        self.combo = QComboBox()
-        self.combo.addItems(PlayerTestData)
-        self.combo.setCurrentIndex(1)
-        layout.addWidget(self.combo)
-        # Select Players
-        #layout.addWidget(QLabel("Team:"))
-        self.combo = QComboBox()
-        self.combo.addItems(PlayerTestData)
-        self.combo.setCurrentIndex(1)
-        layout.addWidget(self.combo)
-        # Select Players
-        #layout.addWidget(QLabel("Team:"))
-        self.combo = QComboBox()
-        self.combo.addItems(PlayerTestData)
-        self.combo.setCurrentIndex(1)
-        layout.addWidget(self.combo)
-     
 
+        # Select Players
+        self.rows = []
+        self.player_container = QVBoxLayout()
+        self.outer.addLayout(self.player_container)
+        self.teambox.currentIndexChanged.connect(
+             lambda idx: (self.players(0) if idx != 0 else None))
+        
+        
+
+        for i in range(1, 10):
+            rowi = QWidget()
+            playerrow = QHBoxLayout(rowi)
+            playerrow.setContentsMargins(0, 0, 0, 0)
+
+            batnum = (QLabel(f"{i}:"))
+            playerbox = QComboBox()
+            playerbox.addItems(PlayerTestData)
+        
+            posbox = QComboBox()
+            posbox.addItems(Pos)
+    
+            playerrow.addWidget(batnum)
+            playerrow.addWidget(playerbox)
+            playerrow.addWidget(posbox)
+
+            rowi.setVisible(False)
+            self.player_container.addWidget(rowi)
+            self.rows.append({"widget": rowi, "batnum": batnum, "player": playerbox, "pos": posbox})
+
+ 
+        #self.rows[0]["widget"].setVisible(True) 
+    
+
+    def players(self,n):
+        self.rows[n]["widget"].setVisible(True) 
+       
+
+     
 
 def main():
     app = QApplication(sys.argv)
