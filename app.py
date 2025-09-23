83bd4c71fb3e25ddaf85e6ca831294901b68c4a9
N6SYNTAX <127181966+N6SYNTAX@users.noreply.github.com>
2025-09-23 18:16:30 +1000

    Added Test data and form

--- app.py
+++ app.py
@@ -1,24 +1,74 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QLineEdit,  QFormLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QLineEdit,  QFormLayout, QComboBox, QVBoxLayout
+from PyQt6.QtCore import Qt
+
+#----- TEST DATA
+TeamTestData = ["C Grade", "C Reserve", "D Grade", "D Reserve", "Womens"]
+PlayerTestData = ["Will David", "David Teakle", "Angus O'loughlin", "Grant Stacomb", "Sheran Medelicot"]
+Pos = ["P","C","1B","2B","3B","SS","LF","CF","RF"]
 
 
-class MainWindow(QMainWindow):
+class Scorecard(QWidget):
     def __init__(self):
         super().__init__()
 
-        layout = QFormLayout()
-        self.setLayout(layout)
+        self.setWindowTitle("DCBC")
+        
+
+        layout = QVBoxLayout(self)
+        #self.setLayout(layout)
+
+
+        # Select Team
+        layout.addWidget(QLabel("Team:"))
+        self.team = QComboBox()
+        self.team.addItems(TeamTestData)
+        self.team.setCurrentIndex(1)
+        layout.addWidget(self.team)
+
+        # Select Players
+        layout.addWidget(QLabel("1:"))
+        self.player1 = QComboBox()
+        self.player1.addItems(PlayerTestData)
+        self.player1.setCurrentIndex(1)
+        layout.addWidget(self.player1)
+
+        self.combo = QComboBox()
+        self.combo.addItems(Pos)
+        self.combo.setCurrentIndex(1)
+        layout.addWidget(self.combo)
+
+
 
         
-        layout.addRow("Name:", QLineEdit(self))
+        self.combo = QComboBox()
+        self.combo.addItems(PlayerTestData)
+        self.combo.setCurrentIndex(1)
+        layout.addWidget(self.combo)
+        # Select Players
+        #layout.addWidget(QLabel("Team:"))
+        self.combo = QComboBox()
+        self.combo.addItems(PlayerTestData)
+        self.combo.setCurrentIndex(1)
+        layout.addWidget(self.combo)
+        # Select Players
+        #layout.addWidget(QLabel("Team:"))
+        self.combo = QComboBox()
+        self.combo.addItems(PlayerTestData)
+        self.combo.setCurrentIndex(1)
+        layout.addWidget(self.combo)
+
+
+
+
+
         
 
 
 def main():
     app = QApplication(sys.argv)
-    win = QWidget()
-    win.setWindowTitle("DCBC")
-    win.resize(600, 400)
+    win = Scorecard()
+    win.resize(600,400)
     win.show()
     sys.exit(app.exec())
 
